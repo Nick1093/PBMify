8ad8262096c820a9b2741ff4f453e5bdc33d461c
ethan <ewcwon1108@gmail.com>
2024-02-05 02:10:40 -0500

    edited navbar.js

--- client/src/Components/navbar.js
+++ client/src/Components/navbar.js
@@ -1,1 +1,16 @@
-import React from "react";
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navBar">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="PBMify"> PBMify</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
