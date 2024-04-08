bfee62a44e6db3e4e21be97e944ed809b066273d
Nicholas Lam <nicholasalexanderlam@gmail.com>
2024-04-08 18:31:11 -0400

    asdf

--- client/src/Pages/UserInterface.js
+++ client/src/Pages/UserInterface.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import PhotoUpload from "../Components/PhotoUpload";
 import Navbar from "../Components/navbar";
-// import { getColourPalette } from "../Components/ColorUtils";
-import { extractColors } from "extract-colors";
 import Image from "./Nick_JA_DP.jpeg";
+import ImageMatrixConverter from "../Components/ImageMatrixConverter";
 
 import "../styles/UserInterface.css";
 
@@ -12,15 +11,6 @@ const UserInterface = () => {
   const [imageData, setImageData] = useState({});
   const [successMessage, setSuccessMsg] = useState("");
   const [paintByNumberImage, setPaintByNumberImage] = useState(null);
-
-  // const src = "Nick_JA_DP.jpeg";
-  const src = Image;
-  extractColors(src)
-    .then((colorPalette) => {
-      console.log(colorPalette);
-      // Now colorPalette is an array of colors extracted from the image
-    })
-    .catch(console.error); // Proper error handling
 
   function allowDrop(event) {
     event.preventDefault();
@@ -117,72 +107,9 @@ const UserInterface = () => {
   };
 
   return (
-    <div className="container">
-      <div
-        id="dropZone"
-        onDragOver={allowDrop}
-        onDrop={drop}
-        style={{
-          width: "600px",
-          height: "400px",
-          border: "2px dashed #aaa",
-          textAlign: "center",
-          lineHeight: "380px",
-          margin: "20px auto",
-        }}
-      >
-        {imageData ? (
-          <img
-            src={imageData}
-            alt="Drag and Drop You Photo"
-            style={{ maxWidth: "90%", maxHeight: "90%" }}
-          />
-        ) : (
-          <>
-            <h1 style={{ color: "lightgray" }}>
-              {/* <link rel="preconnect" href="https://fonts.googleapis.com">
-                       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> */}
-              {/* <link href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Tilt+Warp&display=swap" rel="stylesheet"> */}
-              Drag and Drop Your Photo
-              {/* </link>
-                      </link>
-                      </link> */}
-            </h1>
-          </>
-        )}
-      </div>
-      <label htmlFor="photoInput" id="addPhotoBtn">
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Tilt+Warp&display=swap" rel="stylesheet">
-              Add Photo
-          </link>
-          </link>
-           </link> */}
-        Add Photo
-      </label>
-      <input
-        type="file"
-        id="photoInput"
-        accept="image/*"
-        onChange={displaySelectedPhoto}
-        style={{ display: "none" }}
-      />
-      <button id="removePhotoBtn" onClick={removePhoto}>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Tilt+Warp&display=swap" rel="stylesheet">
-            Remove Photo
-          </link>
-          </link>
-           </link> */}
-        Remove Photo
-      </button>
-      {popupMessage && <div className="popup">{popupMessage}</div>}
-      {/* <button onClick={saveImage}>Save Image</button> */}
-      {successMessage && <div className="saveSuccess">{successMessage}</div>}
-      {imageData && <div></div>}
-    </div>
+    <>
+      <ImageMatrixConverter />
+    </>
   );
 };
 
